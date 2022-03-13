1e1105d0edfd7139d2e659ae6d2addc0ff0ad2b2
z9fr <z9fr@protonmail.com>
2022-03-14 00:04:02 +0530

    changed colors of the login

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -8,26 +8,36 @@ import {
   Input,
   Link,
   Stack,
+  Center, 
   Image,
 } from "@chakra-ui/react";
 
 export default function Login() {
   return (
     <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-      <Flex flex={1}>
-        <Image alt={"Login Image"} objectFit={"cover"} src={"/signin.jpg"} />
+    <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "/signin.jpg"
+          }
+        />
       </Flex>
 
       <Flex p={8} flex={1} align={"center"} justify={"center"}>
         <Stack spacing={4} w={"full"} maxW={"md"}>
-          <Heading fontSize={"2xl"}>Sign in to your account</Heading>
+        <Center>
+                  <Heading fontSize={"4xl"}>Sign In</Heading>
+
+        </Center>
           <FormControl id="email">
             <FormLabel>Email address</FormLabel>
-            <Input type="email" />
+            <Input type="email" size='lg'/>
           </FormControl>
           <FormControl id="password">
             <FormLabel>Password</FormLabel>
-            <Input type="password" />
+            <Input type="password" size='lg'/>
           </FormControl>
           <Stack spacing={6}>
             <Stack
@@ -38,12 +48,12 @@ export default function Login() {
               <Checkbox>Remember me</Checkbox>
               <Link color={"blue.500"}>Forgot password?</Link>
             </Stack>
-            <Button colorScheme={"blue"} variant={"solid"}>
+            <Button colorScheme={"twitter"}  variant={"solid"} size='lg'>
               Sign in
             </Button>
           </Stack>
         </Stack>
       </Flex>
-    </Stack>
+          </Stack>
   );
 }
